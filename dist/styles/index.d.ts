1fde15b75c7d7ec63d50cf8f446495c524f16c05
Dksie09 <dakshiegoel@gmail.com>
2024-07-08 23:32:11 +0530

    cache clean

--- dist/styles/index.d.ts
+++
@@ -1,8 +0,0 @@
-import globalStyles from "./globals.css";
-import ButtonStyles from "../components/Button/Button.module.css";
-import CardStyles from "../components/Card/Card.module.css";
-import DropdownMenuStyles from "../components/DropdownMenu/DropdownMenu.module.css";
-import InputStyles from "../components/Input/Input.module.css";
-import ProgressBarStyles from "../components/ProgressBar/ProgressBar.module.css";
-import TextAreaStyles from "../components/TextArea/TextArea.module.css";
-export { globalStyles, ButtonStyles, CardStyles, DropdownMenuStyles, InputStyles, ProgressBarStyles, TextAreaStyles, };
