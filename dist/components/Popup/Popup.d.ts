17bef84bbd9a298a3d00bc03364d768d5fb9b1b9
Dksie09 <dakshiegoel@gmail.com>
2024-07-09 00:52:25 +0530

    jai mata di

---
+++ dist/components/Popup/Popup.d.ts
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Popup.module.css";
+export interface PopupProps {
+    isOpen: boolean;
+    onClose: () => void;
+    className?: string;
+    children: React.ReactNode;
+    title?: string;
+    closeButtonText?: string;
+    outerBg?: string;
+    innerBg?: string;
+    textColor?: string;
+    borderColor?: string;
+}
+export declare const Popup: React.FC<PopupProps>;
+export { styles as PopupStyles };
+export default Popup;
