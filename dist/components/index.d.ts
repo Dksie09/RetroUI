c7b66e7a368735e42bb7d7ed6f82ff0953170dc5
Dksie09 <dakshiegoel@gmail.com>
2024-07-08 23:32:11 +0530

    cache clean

--- dist/components/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Button/Button";
-export * from "./Card/Card";
-export * from "./DropdownMenu/DropdownMenu";
-export * from "./ProgressBar/ProgressBar";
-export * from "./Popup/Popup";
-export * from "./Input/Input";
-export * from "./TextArea/TextArea";
