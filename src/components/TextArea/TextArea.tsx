84fdb97fc1f1946c89bf4bb0ef2d9e8143d19e09
Dksie09 <dakshiegoel@gmail.com>
2024-07-08 17:05:36 +0530

    test comit

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -18,7 +18,7 @@ export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
     const svgString = useMemo(() => {
       const color =
         borderColor ||
-        "rgb(var(--custom-border-rgb, var(--border-textarea-rgb, 255,255,255)))";
+        "rgb(var(--custom-border-rgb, var(--border-textarea-rgb, 0,0,0)))";
       const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="8" height="8"><path d="M3 1h1v1h-1zM4 1h1v1h-1zM2 2h1v1h-1zM5 2h1v1h-1zM1 3h1v1h-1zM6 3h1v1h-1zM1 4h1v1h-1zM6 4h1v1h-1zM2 5h1v1h-1zM5 5h1v1h-1zM3 6h1v1h-1zM4 6h1v1h-1z" fill="${color}"/></svg>`;
       return `url("data:image/svg+xml,${encodeURIComponent(svg)}")`;
     }, [borderColor]);
